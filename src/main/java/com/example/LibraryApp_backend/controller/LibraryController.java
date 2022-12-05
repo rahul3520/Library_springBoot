cd9fbd6ed8c6405e7ca9e43778029261d52575f9
rahulg3520 <rahul@nestgroup.net>
2022-12-06 00:11:00 +0530

    books entry route

--- src/main/java/com/example/LibraryApp_backend/controller/LibraryController.java
+++ src/main/java/com/example/LibraryApp_backend/controller/LibraryController.java
@@ -1,8 +1,16 @@
 package com.example.LibraryApp_backend.controller;
 
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LibraryController {
+
+
+    @PostMapping("/booksEntry")
+    public String booksEntry()
+    {
+        return "books entry page";
+    }
 }
